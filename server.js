fba16026c598a5b096550a5a9ff588989b7d6ace
Packia Sheela Varadhan <packiasheelav@gmail.com>
2018-11-09 11:27:10 +0200

    server modified

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const analyze = require('./routes/api/analyze');
 
 const app=express();
-const path=require('path');
+// const path=require('path');
 const port=process.env.PORT||5000;
 
 //Body parser middleware
@@ -12,8 +12,6 @@ app.use(bodyParser.json());
 
 //use Routes
 app.use('/api/analyze',analyze);
-app.use(express.static(path.join(__dirname,'client','build')))
-app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'client','build','index.html')));
 
-app.listen(port,()=>console.log('Server started on port ${port}'));
+app.listen(port,()=>console.log(`Server started on port ${port}`));
 
